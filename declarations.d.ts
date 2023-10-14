1fa1f48c8d27f81f1c8f9d448fbfdd78f19fbe61
Jordan Aston <jordanaston93@gmail.com>
2023-10-14 18:54:47 +1000

    initial commit

---
+++ declarations.d.ts
@@ -0,0 +1,3 @@
+// Allows png files to be imported 
+
+declare module "*.png";
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {
+      backgroundColor: {
+        ForumPurple: '#6537FF',
+        ForumLightGray: '#E8E8E8',
+        ForumCharcoal: '#383939',
+      },
+      textColor: {
+        ForumCharcoal: '#383939',
+        ForumPurple: '#6537FF',
+      },
+      borderColor: {
+        ForumPurple: '#6537FF',
+        ForumLightGray: '#E8E8E8',
+        ForumBlack: '010214',
+        ForumCharcoal: '#383939',
+        ForumBlack: '#010214',
+      },
+      fontFamily: {
+        'lexend-thin': ['Lexend-Thin', 'default'],
+        'lexend-extralight': ['Lexend-ExtraLight', 'default'],
+        'lexend-light': ['Lexend-Light', 'default'],
+        'lexend-regular': ['Lexend-Regular', 'default'],
+        'lexend-medium': ['Lexend-Medium', 'default'],
+        'lexend-semibold': ['Lexend-SemiBold', 'default'],
+        'lexend-bold': ['Lexend-Bold', 'default'],
+        'lexend-extrabold': ['Lexend-ExtraBold', 'default'],
+        'lexend-black': ['Lexend-Black', 'default'],
+
+        'syne-regular': ['Syne-Regular', 'default'],
+        'syne-medium': ['Syne-Medium', 'default'],
+        'syne-semibold': ['Syne-SemiBold', 'default'],
+        'syne-extrabold': ['Syne-ExtraBold', 'default'],
+        'syne-bold': ['Syne-Bold', 'default'],
+      },
+    },
+  },
+  plugins: [],
+};
