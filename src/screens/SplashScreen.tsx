5b265b8f7246d6a9cb66cdb996282e836f1c1636
Jordan Aston <jordanaston93@gmail.com>
2023-10-14 21:55:40 +1000

    splash screen created

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {SafeAreaView, View, StatusBar} from 'react-native';
+import RocketLogo from '../assets/svg/RocketLogo';
+import {useNavigation} from '@react-navigation/native';
+import {MainStackParamList} from '../navigation/MainStackNavigator';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {Colors} from '../constants/Colors';
+
+type NavigationList = NativeStackNavigationProp<
+  MainStackParamList,
+  'SplashScreen'
+>;
 
 const SplashScreen: React.FC = (): JSX.Element => {
+  const navigation = useNavigation<NavigationList>();
+
+  setTimeout(() => {
+    navigation.navigate('WelcomeScreen');
+  }, 2000);
+
   return (
-    <SafeAreaView className="flex-1">
+    <SafeAreaView className="flex-1 bg-ForumPurple">
+      <StatusBar barStyle="light-content" />
       <View className="flex-1 justify-center items-center">
-        <Text>Splash Screen</Text>
+        <RocketLogo color={Colors.White} width="120" height="179" />
       </View>
     </SafeAreaView>
   );
