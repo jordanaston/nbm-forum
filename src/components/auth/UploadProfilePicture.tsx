291e09d15bfcbe547a7f893f477690f6d4ffdce2
Jordan Aston <jordanaston93@gmail.com>
2023-10-16 15:49:04 +1000

    skip for now button added to update profile picture screen

--- src/components/auth/UploadProfilePicture.tsx
+++ src/components/auth/UploadProfilePicture.tsx
@@ -16,6 +16,21 @@ const UploadProfilePicture: React.FC<Props> = ({
         description="Let’s put a name to a face. Upload a profile picture to complete your profile. This is an optional step."
       />
       <Button onPress={onNext} text="Create my Account" />
+      <View className="mt-4">
+        <Button
+          onPress={() => {}}
+          text="Skip for now"
+          includeArrow={false}
+          backgroundColor="bg-none"
+          textColor="text-ForumCharcoal"
+          textSize="text-[14px]"
+          fontStyle="font-syne-medium"
+          underline="underline"
+          border=""
+          height=""
+          position=""
+        />
+      </View>
     </View>
   );
 };
