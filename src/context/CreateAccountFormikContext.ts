b128f4a901bd5caed86d80f928e2520273d5d1e2
Jordan Aston <jordanaston93@gmail.com>
2023-10-20 23:19:17 +1000

    update in create account context - removed React. for consistency

--- src/context/CreateAccountFormikContext.ts
+++ src/context/CreateAccountFormikContext.ts
@@ -1,12 +1,12 @@
-import React from 'react';
+import {createContext, useContext} from 'react';
 import {FormikProps} from 'formik';
 import {CreateAccountArgs} from '../types/CreateAccountTypes';
 
 export const FormikContext =
-  React.createContext<FormikProps<CreateAccountArgs> | null>(null);
+  createContext<FormikProps<CreateAccountArgs> | null>(null);
 
 export const useCreateAccountFormik = (): FormikProps<CreateAccountArgs> => {
-  const context = React.useContext(FormikContext);
+  const context = useContext(FormikContext);
   if (!context) {
     throw new Error(
       'useCreateAccountFormik must be used within a FormikContext.Provider',
